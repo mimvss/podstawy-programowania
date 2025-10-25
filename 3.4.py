3e7055c565312df2dafba53da763664258332dd9
s-A301-05 <s-A301-05@CAMPUS.UEK.KRAKOW.PL>
2025-10-25 09:57:46 +0200

    poidstawwy programowania

---
+++ 3.4.py
@@ -0,0 +1,7 @@
+###
+# A program that, for a given speed in km/h,
+# prints the speed in m/s
+#
+speed_kmh = 300
+speed_ms = speed_kmh/3.6
+print("speed in km/h =", speed_kmh, " speed in m/s =", speed_ms)
